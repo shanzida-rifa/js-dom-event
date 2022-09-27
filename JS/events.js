f541ad39cc15e5e0bc1dc6be3973e4f14bb451ef
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-27 13:29:39 +0600

    added

--- JS/events.js
+++ JS/events.js
@@ -1,1 +1,30 @@
 console.log('This is a event file');
+
+// option 1: directly set on the HTML
+
+
+// option 2: add onClick funtion
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// option 3 
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+// option 3 another
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
+
+// option 4 
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
