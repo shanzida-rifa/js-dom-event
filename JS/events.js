ecd618cb9f9ab6a203bf30bc8a44cdfff715bd0d
Shanzida-Rifa <rifashanzida@gmail.com>
2022-09-27 17:22:03 +0600

    added

--- JS/events.js
+++ JS/events.js
@@ -28,3 +28,14 @@ pinkButton.addEventListener('click', makePink);
 function makePink() {
     document.body.style.backgroundColor = 'pink';
 }
+
+// option 4 another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+})
+
+// option 4 final
+document.getElementById('make-goldenrod').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'goldenRod';
+});
